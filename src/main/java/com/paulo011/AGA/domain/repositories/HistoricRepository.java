e092b27ae9b433fbf0d48946314a124e234b8a76
paulo011 <pauloortega011@gmail.com>
2023-12-05 01:51:55 -0300

    [FEAT] introduces three research methods

--- src/main/java/com/paulo011/AGA/domain/repositories/HistoricRepository.java
+++ src/main/java/com/paulo011/AGA/domain/repositories/HistoricRepository.java
@@ -2,12 +2,16 @@ package com.paulo011.AGA.domain.repositories;
 
 import com.paulo011.AGA.domain.entities.Historic;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public interface HistoricRepository {
     void save(Historic historic);
     Historic findById(UUID id);
+    List<Historic> findByUser(String cpf);
+    List<Historic> findByProduct(UUID code);
+    List<Historic> findByDate(Date date);
     List<Historic> findAll();
     void delete(UUID id);
 }
