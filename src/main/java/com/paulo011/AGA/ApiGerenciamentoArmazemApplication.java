a9c89aa58afd3b8ab0e084ac24463a96762e1130
paulo011 <pauloortega011@gmail.com>
2024-01-16 16:33:01 -0300

    [UPDATE] add OpenFeign annotations

--- src/main/java/com/paulo011/AGA/ApiGerenciamentoArmazemApplication.java
+++ src/main/java/com/paulo011/AGA/ApiGerenciamentoArmazemApplication.java
@@ -2,8 +2,10 @@ package com.paulo011.AGA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiGerenciamentoArmazemApplication {
 
 	public static void main(String[] args) {
